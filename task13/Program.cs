3a387efdbef2a185f4075e3e161553b116040d02
Alexandra <lebedeva1618@yandex.ru>
2022-12-02 13:10:33 +0300

    задача 13, вывод третьей цифры

---
+++ task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+string? b = Console.ReadLine();
+
+int x = Int32.Parse(b);
+if (x < 100) 
+    Console.WriteLine("Третьей цифры нет");
+else
+    Console.WriteLine(Convert.ToInt32(b[2].ToString()));
+
