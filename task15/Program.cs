fdb1bc398997eb091eef63510467fe4da0ac3ecf
Alexandra <lebedeva1618@yandex.ru>
2022-12-02 13:24:51 +0300

    задача 15, день недели

---
+++ task15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+int day = new Random(). Next(1, 8);
+Console.Write($"день {day} - это " );
+if (day >= 1 && day < 6)
+    Console.WriteLine("будний день");
+else
+    Console.WriteLine("выходной");
