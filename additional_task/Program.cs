42ee049731c2a4a0fa39e34084b4ff5a6c893546
Alexandra <lebedeva1618@yandex.ru>
2022-12-03 18:14:58 +0300

    Сделана дополнительная задача

---
+++ additional_task/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите последовательность чисел: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int max2 = 0;
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+      
+        if (n > max) 
+        {
+        max2 = max;
+        max = n;
+        }
+        else if (n < max && n > max2)
+        {
+        max2 = n;
+        }
+           
+}
+
+Console.WriteLine("end"); 
+Console.WriteLine(" ");
+Console.WriteLine($"Второе максимальное число - {max2}");
+
+
